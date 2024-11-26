0aecc3208fbfceb37535af495270795310b7de2d
duanegero <duanegero11@gmail.com>
2024-11-26 10:25:06 -0400

    updating password

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'Basketball DataBase',
-    password: 'BasketballAPI',
+    password: 'postgrespassword',
     port: 5433,
 });
 
